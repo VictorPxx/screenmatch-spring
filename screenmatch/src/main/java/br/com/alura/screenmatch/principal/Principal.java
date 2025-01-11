af13d1879d243a2ef7d4a2e4364a8af0496d0c6b
VictorPxx <victordev.luz@gmail.com>
2025-01-11 12:08:52 -0300

    Add classe Episodio, att com uso da API Stream a classe Principal

--- screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,15 +1,17 @@
 package br.com.alura.screenmatch.principal;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.model.EscreveArquivo;
+import br.com.alura.screenmatch.model.*;
 import br.com.alura.screenmatch.service.ConsumirApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -20,25 +22,59 @@ public class Principal {
     private EscreveArquivo escreveArquivo = new EscreveArquivo();
 
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
-    private final String apiKey = dotenv.get("API_KEY");
+    private final String API_KEY = dotenv.get("API_KEY");
 
     public void exibeMenu() {
         System.out.println("Digite o nome da série: ");
         var nomeSerie = scanner.nextLine().replace(" ", "+").trim().toLowerCase();
-        var json = consumo.obterDados(ENDERECO + nomeSerie + "&apikey=" + apiKey);
+        var json = consumo.obterDados(ENDERECO + nomeSerie + "&apikey=" + API_KEY);
         var dadosSerie = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dadosSerie);
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
         for (int i = 1; i <= dadosSerie.totalTemporadas() ; i++) {
-            json = consumo.obterDados(ENDERECO + nomeSerie + "&season=" + i + "&apikey=" + apiKey );
+            json = consumo.obterDados(ENDERECO + nomeSerie + "&season=" + i + "&apikey=" + API_KEY);
             var dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
             temporadas.add(dadosTemporada);
         }
         temporadas.forEach(System.out::println);
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        List<Episodio> episodios = temporadas.stream()
+                        .flatMap(t -> t.episodios().stream().map(d -> new Episodio(t.numeroTemporada(), d)))
+                                .collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano você quer ver os episódios?");
+        var ano = scanner.nextInt();
+        scanner.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano,1,1);
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada()
+                        + " | Episódio: " + e.getTitulo()
+                        + " | Data lançamento: " + e.getDataLancamento().format(formatador)
+                ));
 
         var dadosConvertidosJson = conversor.converteParaJson(temporadas);
 		escreveArquivo.escrever( nomeSerie.replace("+", "-") + ".json", dadosConvertidosJson);
